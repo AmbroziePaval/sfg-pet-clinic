e1f67cb9cfa146015db4ac23dd9e850de94e8b58
Ambrozie <ambroziepaval@gmail.com>
2018-10-07 22:45:56 +0300

    Adding POJOs. Closes #2.

---
+++ src/main/java/com/ambroziepaval/sfgpetclinic/model/PetType.java
@@ -0,0 +1,17 @@
+package com.ambroziepaval.sfgpetclinic.model;
+
+/**
+ * Created by Ambrozie on 07/10/2018
+ */
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/ambroziepaval/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package com.ambroziepaval.sfgpetclinic.model;
+
+/**
+ * Created by Ambrozie on 07/10/2018
+ */
+public class Vet extends Person {
+}

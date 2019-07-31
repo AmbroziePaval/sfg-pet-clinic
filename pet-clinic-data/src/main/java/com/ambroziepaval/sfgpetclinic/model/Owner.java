505b2eedcdea96e876469fa233abd096b9106768
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-07-31 20:59:42 +0300

    Added Visit Entity; updated Owner with Pet relationship. Closes #27

--- pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/model/Owner.java
@@ -1,7 +1,19 @@
 package com.ambroziepaval.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Ambrozie on 07/10/2018
  */
 public class Owner extends Person {
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }

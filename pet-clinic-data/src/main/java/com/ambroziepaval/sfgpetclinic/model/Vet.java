25f2c7eff30f1cdd852d163ba450a69a9b092583
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-07-31 21:03:22 +0300

    Add Vet Specialties. Closes #28

--- pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/model/Vet.java
@@ -1,7 +1,19 @@
 package com.ambroziepaval.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * Created by Ambrozie on 07/10/2018
  */
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }

1eeb18990ee652b8e941a01be4d72657d78fcd15
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-08-18 16:59:30 +0300

    Create Spring Data JPA Service for Visit. Closes #47

--- pet-clinic-web/src/main/java/com/ambroziepaval/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/ambroziepaval/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.ambroziepaval.sfgpetclinic.bootstrap;
 
 import com.ambroziepaval.sfgpetclinic.model.*;
-import com.ambroziepaval.sfgpetclinic.services.OwnerService;
-import com.ambroziepaval.sfgpetclinic.services.PetTypeService;
-import com.ambroziepaval.sfgpetclinic.services.SpecialtyService;
-import com.ambroziepaval.sfgpetclinic.services.VetService;
+import com.ambroziepaval.sfgpetclinic.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -20,16 +17,19 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
+    private final VisitService visitService;
 
     public DataLoader(
             OwnerService ownerService,
             VetService vetService,
             PetTypeService petTypeService,
-            SpecialtyService specialtyService) {
+            SpecialtyService specialtyService,
+            VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
     @Override
@@ -94,6 +94,13 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(fionasCat);
+        catVisit.setDate(LocalDate.now());
+        catVisit.setDescription("Sneezy Kitty");
+
+        visitService.save(catVisit);
 
         System.out.println("Loaded Owners...");
 

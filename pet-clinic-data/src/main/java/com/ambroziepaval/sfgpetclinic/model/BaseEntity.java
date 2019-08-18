4d5432a0023693a1ca6b20d6539b14851c3d7d41
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-08-18 15:36:12 +0300

    Update Base Entity (JPA). Closes #36

--- pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/model/BaseEntity.java
@@ -1,12 +1,19 @@
 package com.ambroziepaval.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  * Created by Ambrozie on 08/10/2018
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {

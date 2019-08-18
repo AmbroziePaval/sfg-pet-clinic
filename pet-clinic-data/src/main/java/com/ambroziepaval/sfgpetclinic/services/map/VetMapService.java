bbba932690dc918f7c0cfe6f544ea9b9775189dc
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-08-18 16:37:45 +0300

    Create Spring Data JPA Specialty Service. Closes #45

--- pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/ambroziepaval/sfgpetclinic/services/map/VetMapService.java
@@ -12,11 +12,11 @@ import java.util.Set;
  * Created by Ambrozie on 09/10/2018
  */
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private SpecialtyService specialtyService;
 
-    public VetServiceMap(SpecialtyService specialtyService) {
+    public VetMapService(SpecialtyService specialtyService) {
         this.specialtyService = specialtyService;
     }
 

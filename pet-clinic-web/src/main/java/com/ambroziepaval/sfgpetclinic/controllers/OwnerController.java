58f2fba9f4ff4d523d7239e7ed87ce89f8682367
Paval, Ambrozie <ambroziepaval@gmail.com>
2019-11-16 13:33:03 +0200

    Display Owner information. Closes #53

--- pet-clinic-web/src/main/java/com/ambroziepaval/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ambroziepaval/sfgpetclinic/controllers/OwnerController.java
@@ -1,10 +1,14 @@
 package com.ambroziepaval.sfgpetclinic.controllers;
 
+import com.ambroziepaval.sfgpetclinic.model.Owner;
+import com.ambroziepaval.sfgpetclinic.model.Pet;
 import com.ambroziepaval.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by Ambrozie on 09/10/2018
@@ -30,5 +34,19 @@ public class OwnerController {
     @GetMapping("/find")
     public String findOwners() {
         return "notimplemented";
+    }
+
+    /**
+     * Custom handler for displaying an owner.
+     *
+     * @param ownerId the ID of the owner to display
+     * @return a ModelMap with the model attributes for the view
+     */
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        Owner owner = ownerService.findById(ownerId);
+        mav.addObject(owner);
+        return mav;
     }
 }
